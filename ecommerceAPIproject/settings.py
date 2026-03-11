0469b5e723020138c03a42dd5df4130bb45add9d
sivasankari-dev <sivasankari.devofficial@gmail.com>
2026-03-11 17:28:00 +0530

    Update settings.py trouble shoot for syntax error in database section

--- ecommerceAPIproject/settings.py
+++ ecommerceAPIproject/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'ecommerceAPIproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
- #   'default': {
+    'default': {
  #       'ENGINE': 'django.db.backends.sqlite3',
  #      'NAME': BASE_DIR / 'db.sqlite3',
     'default': dj_database_url.config(
