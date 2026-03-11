9060cd9ed2e7a6e4bf04c1fe073e8359e087ac01
sivasankari-dev <sivasankari.devofficial@gmail.com>
2026-03-11 19:37:44 +0530

    Update settings.py Adding frontend URL to settings
    Adding frontend url from vercel to settings

--- ecommerceAPIproject/settings.py
+++ ecommerceAPIproject/settings.py
@@ -63,7 +63,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "http://localhost:5173",
-    "https://shoppro.vercel.app"
+    "https://shopprofrontend.vercel.app"
 ]
 
 ROOT_URLCONF = 'ecommerceAPIproject.urls'
