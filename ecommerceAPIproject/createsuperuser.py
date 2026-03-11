2cb47f454d9a17e633709b721116a34a743a4b52
sivasankari-dev <sivasankari.devofficial@gmail.com>
2026-03-11 17:12:54 +0530

    Create createsuperuser.py to use in render

---
+++ ecommerceAPIproject/createsuperuser.py
@@ -0,0 +1,26 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+# REPLACE 'your_project_name' with the folder containing settings.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+django.setup()
+
+def create_admin():
+    User = get_user_model()
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+    email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+    if not password:
+        print("No password set. Skipping superuser creation.")
+        return
+
+    if not User.objects.filter(username=username).exists():
+        print(f"Creating superuser: {username}")
+        User.objects.create_superuser(username=username, email=email, password=password)
+    else:
+        print("Superuser already exists.")
+
+if __name__ == "__main__":
+    create_admin()
