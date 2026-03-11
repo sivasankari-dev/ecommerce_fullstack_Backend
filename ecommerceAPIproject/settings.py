096e032d63f70ab3717a5b9195d06887a786cdea
sivasankari-dev <sivasankari.devofficial@gmail.com>
2026-03-11 14:13:24 +0530

    Adding production url to CORS origin

--- ecommerceAPIproject/settings.py
+++ ecommerceAPIproject/settings.py
@@ -59,6 +59,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "http://localhost:5173"
+    "https://shoppro.vercel.app"
 ]
 
 ROOT_URLCONF = 'ecommerceAPIproject.urls'
