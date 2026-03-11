12d7d6edd0348ae2ce9bfe1b3fa54497c627a852
sivasankari-dev <sivasankari.devofficial@gmail.com>
2026-03-11 17:12:11 +0530

    Update settings.py to add postgres DB instead of sqlite

--- ecommerceAPIproject/settings.py
+++ ecommerceAPIproject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,9 +89,14 @@ WSGI_APPLICATION = 'ecommerceAPIproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #      'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
     }
 }
 
